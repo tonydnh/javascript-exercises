ede0dc045b31ed0eedbe00a61224f2803fc2f489
Tony Dinh <tonydinh442@gmail.com>
2024-05-19 21:55:42 -0500

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let res = ''
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            res += string
+        }
+    } else {
+        return 'ERROR'
+    }
+    return res
 };
 
 // Do not edit below this line
