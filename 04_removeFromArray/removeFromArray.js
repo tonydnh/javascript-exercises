84629105083dea1659a4febc4d0c473a6c07d217
Tony Dinh <tonydinh442@gmail.com>
2024-05-19 22:13:36 -0500

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...list) {
+    for (const itemToRemove of list) {
+        arr = arr.filter(item => item !== itemToRemove)
+    }
+    return arr
+    
+    // return arr.filter(item => !list.includes(item))
 };
 
 // Do not edit below this line
