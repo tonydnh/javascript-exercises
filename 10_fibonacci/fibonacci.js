e5879b44c851ddb87209d66332908e511bd0be8e
Tony Dinh <tonydinh442@gmail.com>
2024-05-31 23:29:41 -0500

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(tgt) {
+    if (tgt < 0) return "OOPS";
+    let nextLatest = 0;
+    let latest = 1;
 
+    for (let i = 0; i < tgt; i++) {
+        let temp = latest + nextLatest;
+        nextLatest = latest;
+        latest = temp;
+    }
+    
+    return nextLatest;
 };
 
 // Do not edit below this line
