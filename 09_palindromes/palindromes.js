9c7c9e37b36772d7c1999bb09cf8ce1422e21ff1
Tony Dinh <tonydinh442@gmail.com>
2024-05-31 22:14:04 -0500

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    str = str
+        .toLowerCase()
+        .split("")
+        .filter(character => alphanumerical.includes(character))
+        .join("");
+
+    for (let i = 0; i <= str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
