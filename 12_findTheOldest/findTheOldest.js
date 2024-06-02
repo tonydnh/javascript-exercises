a24b2f521a27217121cee9401baf3925a4c33095
Tony Dinh <tonydinh442@gmail.com>
2024-06-01 20:15:40 -0500

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = 0;
+    let oldestPerson;
+    arr.forEach(person => {
+        let death = (new Date()).getFullYear();
+        if (person.yearOfDeath) {
+            death = person.yearOfDeath;
+        }
+        let age = death - person.yearOfBirth;
+        if (age > oldest) {
+            oldest = age;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
